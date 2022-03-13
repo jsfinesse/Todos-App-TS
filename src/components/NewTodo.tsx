105bb35fdccf4bd9c1b7c43c10ed1496b070836e
Prithu Singh <prithusingh0327@gmail.com>
2022-03-13 16:36:12 +0530

    Added NewTodo form component

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,20 @@
+import { useRef } from "react";
+
+import React from "react";
+
 const NewTodo = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo text</label>
-            <input type="" id="text"/>
+            <input type="" id="text" ref={todoTextInputRef} />
             <button>Add</button>
         </form>
     );
