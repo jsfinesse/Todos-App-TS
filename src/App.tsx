8f94c8480584c96f3f7cf38a406b6c6b8e6dbc54
Prithu Singh <prithusingh0327@gmail.com>
2022-03-13 17:08:00 +0530

    Added logic to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-    const todos = [new Todo("Get groceries"), new Todo("Complete Assignments")];
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const addHandler = (todoText: string) => {
+        const newTodo = new Todo(todoText);
+
+        setTodos((prevTodos) => {
+            return prevTodos.concat(newTodo);
+        });
+    };
 
     return (
         <div>
+            <NewTodo onAdd={addHandler} />
             <Todos items={todos} />
         </div>
     );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -2,13 +2,19 @@ import { useRef } from "react";
 
 import React from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAdd: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
 
         const enteredText = todoTextInputRef.current!.value;
+
+        if (enteredText.trim().length === 0) {
+            return;
+        }
+
+        props.onAdd(enteredText);
     };
 
     return (
