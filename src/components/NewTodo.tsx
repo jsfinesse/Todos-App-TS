98d47c2db157844c2cfc3d67f7ccbca45acedbea
Prithu Singh <prithusingh0327@gmail.com>
2022-03-13 16:19:35 +0530

    Added TodoItem component

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,11 @@
+const NewTodo = () => {
+    return (
+        <form>
+            <label htmlFor="text">Todo text</label>
+            <input type="" id="text"/>
+            <button>Add</button>
+        </form>
+    );
+};
+
+export default NewTodo;
