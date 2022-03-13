5454bfb9a0129b202f04471dec289366b4f3f3ee
Prithu Singh <prithusingh0327@gmail.com>
2022-03-13 17:57:13 +0530

    Added styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,18 @@
-const TodoItem: React.FC<{text: string}> = (props) => {
-    return <li>{props.text}</li>
+import classes from "./TodoItem.module.css";
+import edit from "../icons/edit.svg";
+import deleteItem from "../icons/deleteItem.svg";
 
+const TodoItem: React.FC<{ text: string }> = (props) => {
+    return (
+        <li className={classes.item}>
+            {props.text}
+            
+            <span>
+                <img src={deleteItem} height={20} alt="delete" />
+            </span>
+            
+        </li>
+    );
 };
 
 export default TodoItem;
